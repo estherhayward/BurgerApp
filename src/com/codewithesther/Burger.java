b7abf988bba73bc51ade26ae40224b7e4c906da1
eawh02 <estherhayward@gmail.com>
2020-08-29 22:50:42 +0200

    Add Burger class, fields and constructor

---
+++ src/com/codewithesther/Burger.java
@@ -0,0 +1,16 @@
+package com.codewithesther;
+
+public class Burger {
+    private String name;
+    private String meatTopping;
+    private String breadType;
+    private double price;
+
+    public Burger(String name, String meatTopping, String breadType, double price) {
+        this.name = name;
+        this.meatTopping = meatTopping;
+        this.breadType = breadType;
+        this.price = price;
+    }
+
+}
--- src/com/codewithesther/Main.java
+++ src/com/codewithesther/Main.java
@@ -3,6 +3,8 @@ package com.codewithesther;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Burger john = new Burger("Delicious Burger", "bacon", "sesame", 12.50);
+
+
     }
 }
