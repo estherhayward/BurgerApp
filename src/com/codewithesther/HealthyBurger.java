1e2db27e1ba4363beb7475c33378109a23f6fe12
eawh02 <estherhayward@gmail.com>
2020-08-30 16:07:39 +0200

    Override addTopping method in HealthyBurger class

--- src/com/codewithesther/HealthyBurger.java
+++ src/com/codewithesther/HealthyBurger.java
@@ -1,9 +1,42 @@
 package com.codewithesther;
 
 public class HealthyBurger extends Burger {
+    private String toppingName5;
+    private double toppingPrice5;
+
+    private String toppingName6;
+    private double toppingPrice6;
+
+    private int count = 1;
 
     public HealthyBurger(String meat, double price) {
         super("Healthy", meat, "brown rye", price);
     }
 
-}
+    @Override
+    public void addTopping(String name, double price) {
+        if (this.count < 5) {
+            System.out.println("count: " + count);
+            this.count++;
+            super.addTopping(name, price);
+        } else if (this.count < 7) {
+            switch (this.count) {
+                case 5:
+                    System.out.println("count: " + this.count);
+                    this.toppingName5 = name;
+                    this.toppingPrice5 = price;
+                    this.count++;
+                    break;
+                case 6:
+                    System.out.println("count: " + this.count);
+                    this.toppingName6 = name;
+                    this.toppingPrice6 = price;
+                    this.count++;
+                    break;
+            }
+        } else {
+                System.out.println("Maximum number of Toppings reached.");
+            }
+        }
+    }
+
--- src/com/codewithesther/Main.java
+++ src/com/codewithesther/Main.java
@@ -3,14 +3,19 @@ package com.codewithesther;
 public class Main {
 
     public static void main(String[] args) {
-        Burger regular = new Burger("Regular", "bacon", "sesame", 12.50);
-        regular.addTopping("lettuce", 0.5);
-        regular.addTopping("tomato", 1);
-        regular.addTopping("mushrooms", 1.2);
-        regular.addTopping("onions", 0.9);
-        regular.addTopping("avocado", 2);
-        System.out.println("You're total order is " + regular.showOrder());
+//        Burger regular = new Burger("Regular", "bacon", "sesame", 12.50);
+//        regular.addTopping("lettuce", 0.5);
+//        regular.addTopping("tomato", 1);
+//        regular.addTopping("mushrooms", 1.2);
+//        regular.addTopping("onions", 0.9);
+//        regular.addTopping("avocado", 2);
+//        System.out.println("You're total order is " + regular.showOrder());
         Burger healthy = new HealthyBurger("soy bacon", 14.50);
+        healthy.addTopping("lettuce", 0.5);
+        healthy.addTopping("tomato", 1);
+        healthy.addTopping("mushrooms", 1.2);
+        healthy.addTopping("onions", 0.9);
+        healthy.addTopping("avocado", 2);
         healthy.showOrder();
 
     }
